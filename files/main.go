22e94e87803a2782cebefe1db97881990a06597c
Jesse Emana <jesseemana@gmail.com>
2023-10-25 11:47:35 +0200

    reading files in GO

--- files/main.go
+++ files/main.go
@@ -3,24 +3,39 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	fmt.Println("Handling files in GO")
 
+	writeFile()
+	readFile("C:\\Users\\hey\\Desktop\\Code\\Golang\\Lessons\\files\\newfile.txt")
+}	
+
+func writeFile() {
 	content := "We've created a second file with Go. Here we are closing with defer."
 	
 	file, err := os.Create("./newfile.txt")
-	if err != nil {
-		panic(err)
-	}
+	checkNilErr(err)
 
 	length, err := io.WriteString(file, content)
-	if err != nil {
-		panic(err)
-	}
+	checkNilErr(err)
 
 	fmt.Println("The length of the file is", length)	
 	defer file.Close()
+}
+
+func readFile(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	checkNilErr(err)
+
+	fmt.Println("Data inside the file \n", string(data))
+}	
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }	
