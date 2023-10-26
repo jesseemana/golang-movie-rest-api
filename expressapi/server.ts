da81d46a972f80fd821b97f74fa8171ed975f9bf
Jesse Emana <jesseemana@gmail.com>
2023-10-26 02:28:35 +0200

    initialized basic node server

---
+++ expressapi/server.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express'
+
+const PORT = 8080
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.status(200).send("Pong")
+})
+
+app.get('/get', (req: Request, res: Response) => {
+  res.status(200).json({ message: "Welcome to Jay's backend REST API" })
+})
+
+app.post('/post', (req: Request, res: Response) => {
+  let myBody = req.body
+  res.status(200).send(myBody)
+})
+
+app.post('/postform', (req: Request, res: Response) => {
+  res.status(200).send(JSON.stringify(req.body))
+})
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))   
